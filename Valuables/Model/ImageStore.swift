35a993120627d3ee1ea7bd4df117be9988f3b146
Theron Jones <theronqj@gmail.com>
2019-07-11 16:01:46 -0700

    End camera, photo choice and storage

--- Valuables/Model/ImageStore.swift
+++ Valuables/Model/ImageStore.swift
@@ -14,13 +14,41 @@ class ImageStore {
     
     func setImage (_ image: UIImage, forKey key: String) {
         cache.setObject(image, forKey: key as NSString)
+        let url = imageURL(forKey: key)
+        if let data = image.jpegData(compressionQuality: 0.5) {
+            let _ = try? data.write(to: url, options: [.atomic])
+        }
     }
     
     func image(forKey key: String) -> UIImage? {
-        return cache.object(forKey: key as NSString)
+        if let existingImage = cache.object(forKey: key as NSString) {
+            return existingImage
+        }
+        
+        let url = imageURL(forKey: key)
+        guard let imageFromDisk = UIImage(contentsOfFile: url.path) else {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: key as NSString)
+        return imageFromDisk
     }
     
     func deleteImage(forKey key: String) {
         cache.removeObject(forKey: key as NSString)
+        
+        let url = imageURL(forKey: key)
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch let deleteError {
+            print("Error removing the image from disk: \(deleteError)")
+        }
+    }
+    
+    func imageURL(forKey key: String) -> URL {
+        let documentDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentDirectories.first!
+        
+        return documentDirectory.appendingPathComponent(key)
     }
 }
